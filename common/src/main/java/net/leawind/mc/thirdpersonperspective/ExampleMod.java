3b3a1c5cd2e953fc0de0c8bbf7e618dbcd4f57a4
LEAWIND <leawind@yeah.net>
2023-11-03 14:46:56 +0800

    Update build scripts
    Extract most of the mod properties into gradle.properties

--- common/src/main/java/net/leawind/mc/thirdpersonperspective/ExampleMod.java
+++ common/src/main/java/net/leawind/mc/thirdpersonperspective/ExampleMod.java
@@ -17,8 +17,7 @@ import java.util.function.Supplier;
 public class ExampleMod {
 	public static final String                            MOD_ID       = "leawind_third_person_perspective";
 	// We can use this if we don't want to use DeferredRegister
-	public static final Supplier<RegistrarManager>        REGISTRIES   = Suppliers.memoize(() -> RegistrarManager.get(
-		MOD_ID));
+	public static final Supplier<RegistrarManager>        REGISTRIES   = Suppliers.memoize(() -> RegistrarManager.get(MOD_ID));
 	// Registering a new creative tab
 	public static final DeferredRegister<CreativeModeTab> TABS         = DeferredRegister.create(MOD_ID,
 																								 Registries.CREATIVE_MODE_TAB);
@@ -27,10 +26,8 @@ public class ExampleMod {
 																						   Component.translatable(
 																							   "itemGroup." + MOD_ID +
 																							   ".example_tab"),
-																						   () -> new ItemStack(
-																							   ExampleMod.EXAMPLE_ITEM.get())));
-	public static final DeferredRegister<Item>            ITEMS        = DeferredRegister.create(MOD_ID,
-																								 Registries.ITEM);
+																						   () -> new ItemStack(ExampleMod.EXAMPLE_ITEM.get())));
+	public static final DeferredRegister<Item>            ITEMS        = DeferredRegister.create(MOD_ID, Registries.ITEM);
 	public static final RegistrySupplier<Item>            EXAMPLE_ITEM = ITEMS.register("example_item",
 																						() -> new Item(new Item.Properties().arch$tab(
 																							ExampleMod.EXAMPLE_TAB)));
