12a9f90f1112eb08418ba07ca4332db7435bdae9
LEAWIND <leawind@yeah.net>
2023-11-03 11:53:01 +0800

    Init commit
    Using architectury now

---
+++ fabric/src/main/java/net/examplemod/fabric/ExampleModFabric.java
@@ -0,0 +1,12 @@
+package net.examplemod.fabric;
+
+
+import net.examplemod.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+	@Override
+	public void onInitialize () {
+		ExampleMod.init();
+	}
+}
