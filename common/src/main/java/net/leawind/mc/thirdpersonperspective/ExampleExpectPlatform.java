60dc707221455e28119e5000813579d237abe1c9
LEAWIND <leawind@yeah.net>
2023-11-03 16:29:47 +0800

    fix: Resource processing in build script

--- common/src/main/java/net/leawind/mc/thirdpersonperspective/ExampleExpectPlatform.java
+++ common/src/main/java/net/leawind/mc/thirdpersonperspective/ExampleExpectPlatform.java
@@ -22,7 +22,6 @@ public class ExampleExpectPlatform {
 	 */
 	@ExpectPlatform
 	public static Path getConfigDirectory () {
-		// Just throw an error, the content should get replaced at runtime.
 		throw new AssertionError();
 	}
 }
