22d756e3b3a26f4d17baec8cb9f4a507b54ea2fe
LEAWIND <leawind@yeah.net>
2023-11-05 22:36:50 +0800

    Add config about distance

--- common/src/main/java/net/leawind/mc/thirdpersonperspective/config/Config.java
+++ common/src/main/java/net/leawind/mc/thirdpersonperspective/config/Config.java
@@ -2,6 +2,7 @@ package net.leawind.mc.thirdpersonperspective.config;
 
 
 import com.mojang.logging.LogUtils;
+import net.leawind.mc.util.monolist.MonoList;
 import org.slf4j.Logger;
 
 public class Config {
@@ -10,10 +11,21 @@ public class Config {
 	public static       boolean isLoaded                   = false;
 	public static       boolean is_mod_enable              = true;
 	public static       boolean is_third_person_by_default = false;
+	public static       int     available_distance_count   = 32;
+	public static       double  distance_min               = 0.5;
+	public static       double  distance_max               = 128;
 
 	// Invoked by Forge or Fabric
 	public static void onLoad (final Object event) {
-		isLoaded = true;
+		distanceMonoList = new MonoList(available_distance_count, i -> {
+			double b = Math.log(distance_min);
+			double a = Math.log(distance_max) - b;
+			return Math.exp(a * i / available_distance_count + b);
+		});
+		isLoaded         = true;
 		LOGGER.info("Config is loaded, event: {}", event);
 	}
+
+	// Generated configurations
+	public static MonoList distanceMonoList;
 }
