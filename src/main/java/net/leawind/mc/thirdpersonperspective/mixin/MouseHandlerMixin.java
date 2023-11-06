c2feb2a5ba55f57d3499eebefa04333009fe3e45
LEAWIND <leawind@yeah.net>
2023-11-06 10:28:46 +0800

    fix: not using Mixin Redirect any more

--- src/main/java/net/leawind/mc/thirdpersonperspective/mixin/MouseHandlerMixin.java
+++ src/main/java/net/leawind/mc/thirdpersonperspective/mixin/MouseHandlerMixin.java
@@ -2,10 +2,12 @@ package net.leawind.mc.thirdpersonperspective.mixin;
 
 
 import net.leawind.mc.thirdpersonperspective.agent.CameraAgent;
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
  * 当玩家尝试转动视角时，
@@ -16,12 +18,25 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  */
 @Mixin(net.minecraft.client.MouseHandler.class)
 public abstract class MouseHandlerMixin {
-	@Redirect(method="turnPlayer", at=@At(value="INVOKE", target="Lnet/minecraft/client/player/LocalPlayer;turn(DD)V"))
-	public void inject_turnPlayer (LocalPlayer player, double y, double x) {
+	@Inject(method="turnPlayer()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/player/LocalPlayer;turn(DD)V"),
+			locals=LocalCapture.CAPTURE_FAILHARD, cancellable=true)
+	private void getLocalX (CallbackInfo ci,
+							double d1,
+							double d2,
+							double d3,
+							double d4,
+							double d5,
+							double d6,
+							double d7,
+							int m) {
+		//		System.out.printf("\r TurnPlayer: %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f", d1, d2, d3, d4, d5, d6, d7);
 		if (CameraAgent.isAvailable() && CameraAgent.getInstance().isFreeTpv) {
-			CameraAgent.getInstance().onCameraTurn(y, x);
-		} else {
-			player.turn(y, x);
+			double dx = d6, dy = d7;
+			if (Minecraft.getInstance().options.invertYMouse().get()) {
+				dy = -dy;
+			}
+			CameraAgent.getInstance().onCameraTurn(dx, dy);
+			ci.cancel();
 		}
 	}
 }
