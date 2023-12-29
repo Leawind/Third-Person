a0e9a418eb9dc03c2bd00c646c45fc01282ef88d
leawind <leawind@yeah.net>
2023-12-29 21:43:26 +0800

    move ConfigControllers upper

---
+++ common/src/main/java/net/leawind/mc/thirdperson/config/ConfigControllers.java
@@ -0,0 +1,26 @@
+package net.leawind.mc.thirdperson.config;
+
+
+import dev.isxander.yacl3.api.Option;
+import dev.isxander.yacl3.api.controller.ControllerBuilder;
+import dev.isxander.yacl3.api.controller.DoubleSliderControllerBuilder;
+import net.minecraft.network.chat.Component;
+
+/**
+ * Config 控制器
+ */
+class ConfigControllers {
+	public static ControllerBuilder<Double> SMOOTH_FACTOR (Option<Double> opt) {
+		return DoubleSliderControllerBuilder.create(opt)
+											.valueFormatter(v -> Component.literal(String.format("1E-%2.2f", v)))
+											.range(0D, 20D)
+											.step(0.25d);
+	}
+
+	public static ControllerBuilder<Double> OFFSET (Option<Double> opt) {
+		return DoubleSliderControllerBuilder.create(opt)
+											.valueFormatter(v -> Component.literal(String.format("%+1.3f", v)))
+											.range(-1D, 1D)
+											.step(0.001d);
+	}
+}
