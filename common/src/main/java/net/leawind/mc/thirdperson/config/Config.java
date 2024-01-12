168b561a066b2174f6b5bba1e93ebfafb7eef4b2
leawind <leawind@yeah.net>
2024-01-12 17:31:32 +0800

    add config: normal/aiming_is_centered

--- common/src/main/java/net/leawind/mc/thirdperson/config/Config.java
+++ common/src/main/java/net/leawind/mc/thirdperson/config/Config.java
@@ -32,39 +32,48 @@ public class Config {
 
 	// ============================================================ //
 	@Expose public boolean            is_mod_enable                             = true;
-	@Expose public boolean            player_rotate_with_camera_when_not_aiming = false;
-	@Expose public boolean            render_crosshair_when_not_aiming          = true;
-	@Expose public boolean            render_crosshair_when_aiming              = true;
-	@Expose public boolean            center_offset_when_flying                 = true;
-	@Expose public boolean            rotate_to_moving_direction                = true;
 	@Expose public boolean            lock_camera_pitch_angle                   = false;
-	@Expose public boolean            turn_with_camera_when_enter_first_person  = true;
-	@Expose public boolean            player_fade_out_enabled                   = true;
-	@Expose public double             camera_ray_trace_length                   = 256;
+	//
+	@Expose public boolean            player_rotate_with_camera_when_not_aiming = false;
+	@Expose public boolean            rotate_to_moving_direction                = true;
 	@Expose public boolean            auto_rotate_interacting                   = true;
 	@Expose public boolean            rotate_interacting_type                   = true;
+	//
 	@Expose public int                available_distance_count                  = 16;
 	@Expose public double             camera_distance_min                       = 0.5;
 	@Expose public double             camera_distance_max                       = 8;
-	// Smooth factors
+	//-------------------------------------------------------------
+	@Expose public boolean            center_offset_when_flying                 = true;
+	@Expose public boolean            turn_with_camera_when_enter_first_person  = true;
+	@Expose public double             camera_ray_trace_length                   = 256;
+	//
+	@Expose public boolean            player_fade_out_enabled                   = true;
+	//
+	@Expose public boolean            render_crosshair_when_not_aiming          = true;
+	@Expose public boolean            render_crosshair_when_aiming              = true;
+	//-------------------------------------------------------------
 	@Expose public double             flying_smooth_factor                      = 0.5;
+	//
 	@Expose public double             adjusting_camera_offset_smooth_factor     = 0.100;
 	@Expose public double             adjusting_distance_smooth_factor          = 0.100;
+	//
 	@Expose public double             normal_smooth_factor_horizon              = 0.500;
 	@Expose public double             normal_smooth_factor_vertical             = 0.500;
 	@Expose public double             normal_camera_offset_smooth_factor        = 0.500;
 	@Expose public double             normal_distance_smooth_factor             = 0.640;
+	//
 	@Expose public double             aiming_smooth_factor_horizon              = 0.002;
 	@Expose public double             aiming_smooth_factor_vertical             = 0.002;
 	@Expose public double             aiming_camera_offset_smooth_factor        = 0.100;
 	@Expose public double             aiming_distance_smooth_factor             = 0.110;
-	// Camera offsets
-	@Expose public boolean            normal_is_centered                        = false;
+	//-------------------------------------------------------------
+	@Expose public boolean            normal_is_centered                        = false;//TODO
 	@Expose public double             normal_max_distance                       = 2.5;
 	@Expose public double             normal_offset_x                           = -0.28;
 	@Expose public double             normal_offset_y                           = 0.31;
 	@Expose public double             normal_offset_center                      = 0.24D;
-	@Expose public boolean            aiming_is_centered                        = false;
+	//
+	@Expose public boolean            aiming_is_centered                        = false;//TODO
 	@Expose public double             aiming_max_distance                       = 0.89;
 	@Expose public double             aiming_offset_x                           = -0.47;
 	@Expose public double             aiming_offset_y                           = -0.09;
--- fabric/src/main/java/net/leawind/mc/thirdperson/fabric/config/ConfigBuilders.java
+++ fabric/src/main/java/net/leawind/mc/thirdperson/fabric/config/ConfigBuilders.java
@@ -26,18 +26,20 @@ public class ConfigBuilders {
 		//==============================//
 		final ConfigCategory CATEGORY_GENERAL = builder.getOrCreateCategory(ConfigManager.getText("option_category.general"));
 		{
-			CATEGORY_GENERAL.addEntry(buildBooleanEntry("is_mod_enable", true, config.is_mod_enable, v -> config.is_mod_enable = v, config, entryBuilder));
-			CATEGORY_GENERAL.addEntry(buildBooleanEntry("lock_camera_pitch_angle", false, config.lock_camera_pitch_angle, v -> config.lock_camera_pitch_angle = v, config, entryBuilder));
+			CATEGORY_GENERAL.addEntry(buildBooleanEntry(true, "is_mod_enable", config.is_mod_enable, v -> config.is_mod_enable = v, config, entryBuilder));
+			CATEGORY_GENERAL.addEntry(buildBooleanEntry(false, "lock_camera_pitch_angle", config.lock_camera_pitch_angle, v -> config.lock_camera_pitch_angle = v, config, entryBuilder));
 			// SubCategory: Player Rotation
-			final SubCategoryBuilder SUBCATEGORY_PLAYER_ROTATION = entryBuilder.startSubCategory(ConfigManager.getText("option_group.player_rotation"));
-			SUBCATEGORY_PLAYER_ROTATION.add(buildBooleanEntry("player_rotate_with_camera_when_not_aiming", false, config.player_rotate_with_camera_when_not_aiming, v -> config.player_rotate_with_camera_when_not_aiming = v, config, entryBuilder));
-			SUBCATEGORY_PLAYER_ROTATION.add(buildBooleanEntry("rotate_to_moving_direction", true, config.rotate_to_moving_direction, v -> config.rotate_to_moving_direction = v, config, entryBuilder));
+			final SubCategoryBuilder SUBCATEGORY_PLAYER_ROTATION = buildSubCategory("player_rotation", entryBuilder);
+			SUBCATEGORY_PLAYER_ROTATION.add(buildBooleanEntry(false, "player_rotate_with_camera_when_not_aiming", config.player_rotate_with_camera_when_not_aiming, v -> config.player_rotate_with_camera_when_not_aiming = v, config, entryBuilder));
+			SUBCATEGORY_PLAYER_ROTATION.add(buildBooleanEntry(true, "rotate_to_moving_direction", config.rotate_to_moving_direction, v -> config.rotate_to_moving_direction = v, config, entryBuilder));
+			SUBCATEGORY_PLAYER_ROTATION.add(buildBooleanEntry(true, "auto_rotate_interacting", config.auto_rotate_interacting, v -> config.auto_rotate_interacting = v, config, entryBuilder));
+			SUBCATEGORY_PLAYER_ROTATION.add(buildBooleanEntry(true, "rotate_interacting_type", config.rotate_interacting_type, v -> config.rotate_interacting_type = v, config, entryBuilder));
 			CATEGORY_GENERAL.addEntry(SUBCATEGORY_PLAYER_ROTATION.build());
-			// SubCategory: Player Rotation
-			final SubCategoryBuilder SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT = entryBuilder.startSubCategory(ConfigManager.getText("option_group.camera_distance_adjustment"));
-			SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT.add(buildIntSliderEntry("available_distance_count", 16, 2, 64, config.available_distance_count, v -> config.available_distance_count = v, config, entryBuilder));
-			SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT.add(buildDoubleEntry("camera_distance_min", 1.0, 0.5, 2.0, config.camera_distance_min, v -> config.camera_distance_min = v, config, entryBuilder));
-			SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT.add(buildDoubleEntry("camera_distance_max", 16D, 2.0, 16D, config.camera_distance_max, v -> config.camera_distance_max = v, config, entryBuilder));
+			// SubCategory: Camera Distance Adjustment
+			final SubCategoryBuilder SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT = buildSubCategory("camera_distance_adjustment", entryBuilder);
+			SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT.add(buildIntSliderEntry(16, "available_distance_count", 2, 64, config.available_distance_count, v -> config.available_distance_count = v, config, entryBuilder));
+			SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT.add(buildDoubleEntry(1.0, "camera_distance_min", 0.5, 2.0, config.camera_distance_min, v -> config.camera_distance_min = v, config, entryBuilder));
+			SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT.add(buildDoubleEntry(16D, "camera_distance_max", 2.0, 16D, config.camera_distance_max, v -> config.camera_distance_max = v, config, entryBuilder));
 			CATEGORY_GENERAL.addEntry(SUBCATEGORY_CAMERA_DISTANCE_ADJUSTMENT.build());
 		}
 		//==============================//
@@ -45,21 +47,16 @@ public class ConfigBuilders {
 		//==============================
 		final ConfigCategory category_misc = builder.getOrCreateCategory(ConfigManager.getText("option_category.misc"));
 		{
-			category_misc.addEntry(buildBooleanEntry("center_offset_when_flying", true, config.center_offset_when_flying, v -> config.center_offset_when_flying = v, config, entryBuilder));
-			category_misc.addEntry(buildBooleanEntry("turn_with_camera_when_enter_first_person", true, config.turn_with_camera_when_enter_first_person, v -> config.turn_with_camera_when_enter_first_person = v, config, entryBuilder));
-			category_misc.addEntry(buildDoubleEntry("camera_ray_trace_length", 256, 32D, 2048D, config.camera_ray_trace_length, v -> config.camera_ray_trace_length = v, config, entryBuilder));
-			// SubCategory: Behavior Interecting
-			final SubCategoryBuilder SUBCATEGORY_BEHAVIOR_INTERACTING = buildSubCategory("behavior_interacting", entryBuilder);
-			SUBCATEGORY_BEHAVIOR_INTERACTING.add(buildBooleanEntry("auto_rotate_interacting", true, config.auto_rotate_interacting, v -> config.auto_rotate_interacting = v, config, entryBuilder));
-			SUBCATEGORY_BEHAVIOR_INTERACTING.add(buildBooleanEntry("rotate_interacting_type", true, config.rotate_interacting_type, v -> config.rotate_interacting_type = v, config, entryBuilder));
-			category_misc.addEntry(SUBCATEGORY_BEHAVIOR_INTERACTING.build());
+			category_misc.addEntry(buildBooleanEntry(true, "center_offset_when_flying", config.center_offset_when_flying, v -> config.center_offset_when_flying = v, config, entryBuilder));
+			category_misc.addEntry(buildBooleanEntry(true, "turn_with_camera_when_enter_first_person", config.turn_with_camera_when_enter_first_person, v -> config.turn_with_camera_when_enter_first_person = v, config, entryBuilder));
+			category_misc.addEntry(buildDoubleEntry(256, "camera_ray_trace_length", 32D, 2048D, config.camera_ray_trace_length, v -> config.camera_ray_trace_length = v, config, entryBuilder));
 			// SubCategory: Player Fade out
 			final SubCategoryBuilder SUBCATEGORY_PLAYER_FADE_OUT = buildSubCategory("player_fade_out", entryBuilder);
-			SUBCATEGORY_PLAYER_FADE_OUT.add(buildBooleanEntry("player_fade_out_enabled", true, config.player_fade_out_enabled, v -> config.player_fade_out_enabled = v, config, entryBuilder));
+			SUBCATEGORY_PLAYER_FADE_OUT.add(buildBooleanEntry(true, "player_fade_out_enabled", config.player_fade_out_enabled, v -> config.player_fade_out_enabled = v, config, entryBuilder));
 			category_misc.addEntry(SUBCATEGORY_PLAYER_FADE_OUT.build());
-			// SubCategory: Player Fade out
+			// SubCategory: Crosshair
 			final SubCategoryBuilder SUBCATEGORY_CROSSHAIR = buildSubCategory("crosshair", entryBuilder);
-			SUBCATEGORY_CROSSHAIR.add(buildBooleanEntry("render_crosshair_when_not_aiming", true, config.render_crosshair_when_not_aiming, v -> config.render_crosshair_when_not_aiming = v, config, entryBuilder));
+			SUBCATEGORY_CROSSHAIR.add(buildBooleanEntry(true, "render_crosshair_when_not_aiming", config.render_crosshair_when_not_aiming, v -> config.render_crosshair_when_not_aiming = v, config, entryBuilder));
 			category_misc.addEntry(SUBCATEGORY_CROSSHAIR.build());
 		}
 		//==============================//
@@ -67,25 +64,25 @@ public class ConfigBuilders {
 		//==============================//
 		final ConfigCategory CATEGORY_SMOOTH_FACTORS = builder.getOrCreateCategory(ConfigManager.getText("option_category.smooth_factors"));
 		{
-			category_misc.addEntry(buildSmoothFactorEntry("flying_smooth_factor", 0.5, config.flying_smooth_factor, v -> config.flying_smooth_factor = v, config, entryBuilder));
+			category_misc.addEntry(buildSmoothFactorEntry(0.5, "flying_smooth_factor", config.flying_smooth_factor, v -> config.flying_smooth_factor = v, config, entryBuilder));
 			// SubCategory: Adjusting Camera
 			final SubCategoryBuilder SUBCATEGORY_ADJUSTING_CAMERA = buildSubCategory("adjusting_camera", entryBuilder);
-			SUBCATEGORY_ADJUSTING_CAMERA.add(buildSmoothFactorEntry("adjusting_camera_offset_smooth_factor", 0.1, config.adjusting_camera_offset_smooth_factor, v -> config.adjusting_camera_offset_smooth_factor = v, config, entryBuilder));
-			SUBCATEGORY_ADJUSTING_CAMERA.add(buildSmoothFactorEntry("adjusting_distance_smooth_factor", 0.1, config.adjusting_distance_smooth_factor, v -> config.adjusting_distance_smooth_factor = v, config, entryBuilder));
+			SUBCATEGORY_ADJUSTING_CAMERA.add(buildSmoothFactorEntry(0.1, "adjusting_camera_offset_smooth_factor", config.adjusting_camera_offset_smooth_factor, v -> config.adjusting_camera_offset_smooth_factor = v, config, entryBuilder));
+			SUBCATEGORY_ADJUSTING_CAMERA.add(buildSmoothFactorEntry(0.1, "adjusting_distance_smooth_factor", config.adjusting_distance_smooth_factor, v -> config.adjusting_distance_smooth_factor = v, config, entryBuilder));
 			CATEGORY_SMOOTH_FACTORS.addEntry(SUBCATEGORY_ADJUSTING_CAMERA.build());
 			// SubCategory: Normal Mode
 			final SubCategoryBuilder SUBCATEGORY_NORMAL_MODE = buildSubCategory("normal_mode", entryBuilder);
-			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry("smooth_factor_horizon", 0.5, config.normal_smooth_factor_horizon, v -> config.normal_smooth_factor_horizon = v, config, entryBuilder));
-			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry("smooth_factor_vertical", 0.5, config.normal_smooth_factor_vertical, v -> config.normal_smooth_factor_vertical = v, config, entryBuilder));
-			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry("camera_offset_smooth_factor", 0.5, config.normal_camera_offset_smooth_factor, v -> config.normal_camera_offset_smooth_factor = v, config, entryBuilder));
-			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry("distance_smooth_factor", 0.64, config.normal_distance_smooth_factor, v -> config.normal_distance_smooth_factor = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry(0.500, "smooth_factor_horizon", config.normal_smooth_factor_horizon, v -> config.normal_smooth_factor_horizon = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry(0.500, "smooth_factor_vertical", config.normal_smooth_factor_vertical, v -> config.normal_smooth_factor_vertical = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry(0.500, "camera_offset_smooth_factor", config.normal_camera_offset_smooth_factor, v -> config.normal_camera_offset_smooth_factor = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildSmoothFactorEntry(0.640, "distance_smooth_factor", config.normal_distance_smooth_factor, v -> config.normal_distance_smooth_factor = v, config, entryBuilder));
 			CATEGORY_SMOOTH_FACTORS.addEntry(SUBCATEGORY_NORMAL_MODE.build());
 			// SubCategory: Aiming Mode
 			final SubCategoryBuilder SUBCATEGORY_AIMING_MODE = buildSubCategory("aiming_mode", entryBuilder);
-			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry("smooth_factor_horizon", 0.002, config.aiming_smooth_factor_horizon, v -> config.aiming_smooth_factor_horizon = v, config, entryBuilder));
-			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry("smooth_factor_vertical", 0.002, config.aiming_smooth_factor_vertical, v -> config.aiming_smooth_factor_vertical = v, config, entryBuilder));
-			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry("camera_offset_smooth_factor", 0.1, config.aiming_camera_offset_smooth_factor, v -> config.aiming_camera_offset_smooth_factor = v, config, entryBuilder));
-			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry("distance_smooth_factor", 0.11, config.aiming_distance_smooth_factor, v -> config.aiming_distance_smooth_factor = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry(0.002, "smooth_factor_horizon", config.aiming_smooth_factor_horizon, v -> config.aiming_smooth_factor_horizon = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry(0.002, "smooth_factor_vertical", config.aiming_smooth_factor_vertical, v -> config.aiming_smooth_factor_vertical = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry(0.100, "camera_offset_smooth_factor", config.aiming_camera_offset_smooth_factor, v -> config.aiming_camera_offset_smooth_factor = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildSmoothFactorEntry(0.110, "distance_smooth_factor", config.aiming_distance_smooth_factor, v -> config.aiming_distance_smooth_factor = v, config, entryBuilder));
 			CATEGORY_SMOOTH_FACTORS.addEntry(SUBCATEGORY_AIMING_MODE.build());
 		}
 		//==============================//
@@ -95,17 +92,19 @@ public class ConfigBuilders {
 		{
 			// SubCategory: Normal Mode
 			final SubCategoryBuilder SUBCATEGORY_NORMAL_MODE = buildSubCategory("normal_mode", entryBuilder);
-			SUBCATEGORY_NORMAL_MODE.add(buildDoubleEntry("max_distance", 2.5, 0.5, 32, config.normal_max_distance, v -> config.normal_max_distance = v, config, entryBuilder));
-			SUBCATEGORY_NORMAL_MODE.add(buildOffsetRatioEntry("offset_x", -0.28, config.normal_offset_x, v -> config.normal_offset_x = v, config, entryBuilder));
-			SUBCATEGORY_NORMAL_MODE.add(buildOffsetRatioEntry("offset_y", 0.31, config.normal_offset_x, v -> config.normal_offset_x = v, config, entryBuilder));
-			SUBCATEGORY_NORMAL_MODE.add(buildOffsetRatioEntry("offset_center", 0.24, config.normal_offset_x, v -> config.normal_offset_x = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildDoubleEntry(+2.50, "max_distance", 0.5, 32, config.normal_max_distance, v -> config.normal_max_distance = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildDoubleEntry(-0.28, "offset_x", -1, +1, config.normal_offset_x, v -> config.normal_offset_x = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildDoubleEntry(+0.31, "offset_y", -1, +1, config.normal_offset_y, v -> config.normal_offset_y = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildBooleanEntry(false, "is_centered", config.normal_is_centered, v -> config.normal_is_centered = v, config, entryBuilder));
+			SUBCATEGORY_NORMAL_MODE.add(buildDoubleEntry(+0.24, "offset_center", -1, +1, config.normal_offset_center, v -> config.normal_offset_center = v, config, entryBuilder));
 			CATEGORY_CAMERA_OFFSET.addEntry(SUBCATEGORY_NORMAL_MODE.build());
 			// SubCategory: Aiming Mode
 			final SubCategoryBuilder SUBCATEGORY_AIMING_MODE = buildSubCategory("aiming_mode", entryBuilder);
-			SUBCATEGORY_AIMING_MODE.add(buildDoubleEntry("max_distance", 0.89, 0.5, 32, config.aiming_max_distance, v -> config.aiming_max_distance = v, config, entryBuilder));
-			SUBCATEGORY_AIMING_MODE.add(buildOffsetRatioEntry("offset_x", -0.47, config.aiming_offset_x, v -> config.aiming_offset_x = v, config, entryBuilder));
-			SUBCATEGORY_AIMING_MODE.add(buildOffsetRatioEntry("offset_y", -0.09, config.aiming_offset_x, v -> config.aiming_offset_x = v, config, entryBuilder));
-			SUBCATEGORY_AIMING_MODE.add(buildOffsetRatioEntry("offset_center", 0.48, config.aiming_offset_x, v -> config.aiming_offset_x = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildDoubleEntry(+0.89, "max_distance", 0.5, 32, config.aiming_max_distance, v -> config.aiming_max_distance = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildDoubleEntry(-0.47, "offset_x", -1, +1, config.aiming_offset_x, v -> config.aiming_offset_x = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildDoubleEntry(-0.09, "offset_y", -1, +1, config.aiming_offset_y, v -> config.aiming_offset_y = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildBooleanEntry(false, "is_centered", config.aiming_is_centered, v -> config.aiming_is_centered = v, config, entryBuilder));
+			SUBCATEGORY_AIMING_MODE.add(buildDoubleEntry(+0.48, "offset_center", -1, +1, config.aiming_offset_center, v -> config.aiming_offset_center = v, config, entryBuilder));
 			CATEGORY_CAMERA_OFFSET.addEntry(SUBCATEGORY_AIMING_MODE.build());
 		}
 		return builder.build();
@@ -117,7 +116,7 @@ public class ConfigBuilders {
 			.setTooltip(ConfigManager.getText("option_group." + name + ".desc"));
 	}
 
-	public static BooleanListEntry buildBooleanEntry (String name, boolean defaultValue, boolean currentValue, Consumer<Boolean> setter, Config config, ConfigEntryBuilder entryBuilder) {
+	public static BooleanListEntry buildBooleanEntry (boolean defaultValue, String name, boolean currentValue, Consumer<Boolean> setter, Config config, ConfigEntryBuilder entryBuilder) {
 		return entryBuilder.startBooleanToggle(ConfigManager.getText("option." + name), currentValue)
 			.setTooltip(ConfigManager.getText("option." + name + ".desc"))
 			.setDefaultValue(defaultValue)
@@ -125,7 +124,7 @@ public class ConfigBuilders {
 			.build();
 	}
 
-	public static DoubleListEntry buildDoubleEntry (String name, double defaultValue, double min, double max, double currentValue, Consumer<Double> setter, Config config, ConfigEntryBuilder entryBuilder) {
+	public static DoubleListEntry buildDoubleEntry (double defaultValue, String name, double min, double max, double currentValue, Consumer<Double> setter, Config config, ConfigEntryBuilder entryBuilder) {
 		return entryBuilder.startDoubleField(ConfigManager.getText("option." + name), currentValue)
 			.setTooltip(ConfigManager.getText("option." + name + ".desc"))
 			.setDefaultValue(defaultValue)
@@ -135,7 +134,7 @@ public class ConfigBuilders {
 			.build();
 	}
 
-	public static IntegerSliderEntry buildIntSliderEntry (String name, int defaultValue, int min, int max, int currentValue, Consumer<Integer> setter, Config config, ConfigEntryBuilder entryBuilder) {
+	public static IntegerSliderEntry buildIntSliderEntry (int defaultValue, String name, int min, int max, int currentValue, Consumer<Integer> setter, Config config, ConfigEntryBuilder entryBuilder) {
 		return entryBuilder.startIntSlider(ConfigManager.getText("option." + name), defaultValue, min, max)
 			.setTooltip(ConfigManager.getText("option." + name + ".desc"))
 			.setDefaultValue(defaultValue)
@@ -143,11 +142,7 @@ public class ConfigBuilders {
 			.build();
 	}
 
-	public static DoubleListEntry buildSmoothFactorEntry (String name, double defaultValue, double currentValue, Consumer<Double> setter, Config config, ConfigEntryBuilder entryBuilder) {
-		return buildDoubleEntry(name, defaultValue, 0, 1, currentValue, setter, config, entryBuilder);
-	}
-
-	public static DoubleListEntry buildOffsetRatioEntry (String name, double defaultValue, double currentValue, Consumer<Double> setter, Config config, ConfigEntryBuilder entryBuilder) {
-		return buildDoubleEntry(name, defaultValue, -1, 1, currentValue, setter, config, entryBuilder);
+	public static DoubleListEntry buildSmoothFactorEntry (double defaultValue, String name, double currentValue, Consumer<Double> setter, Config config, ConfigEntryBuilder entryBuilder) {
+		return buildDoubleEntry(defaultValue, name, 0, 1, currentValue, setter, config, entryBuilder);
 	}
 }
