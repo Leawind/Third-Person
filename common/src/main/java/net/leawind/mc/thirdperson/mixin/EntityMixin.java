f246c7e3faa86fa49b178fbe79d73e2a3851dfc1
leawind <leawind@yeah.net>
2024-02-21 21:52:23 +0800

    feat: Entity#pick return MISS when hitResult distance is too far.

--- common/src/main/java/net/leawind/mc/thirdperson/mixin/EntityMixin.java
+++ common/src/main/java/net/leawind/mc/thirdperson/mixin/EntityMixin.java
@@ -2,15 +2,22 @@ package net.leawind.mc.thirdperson.mixin;
 
 
 import net.leawind.mc.thirdperson.ThirdPerson;
+import net.leawind.mc.thirdperson.ThirdPersonConstants;
 import net.leawind.mc.util.math.LMath;
 import net.leawind.mc.util.math.vector.api.Vector3d;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * 方法{@link Entity#pick}用于计算玩家视线落点
@@ -60,5 +67,29 @@ public class EntityMixin {
 		} else {
 			return viewVectorFake;
 		}
+	}
+
+	/**
+	 * 限制距离为 {@link ThirdPersonConstants#MAX_INTERACTION_DISTANCE}
+	 * <p>
+	 * 通过检查玩家的眼睛位置与选定实体或方块之间的距离来修改其返回值。如果距离大于预定义的最大交互距离，则修改返回的HitResult以指示未命中。
+	 */
+	@Inject(method="pick", at=@At("RETURN"), cancellable=true)
+	public void pick_return (double pickRange, float partialTick, boolean includeFluid, CallbackInfoReturnable<HitResult> ci) {
+		HitResult hitResult = ci.getReturnValue();
+		if (hitResult instanceof EntityHitResult entityHitResult) {
+			Entity entity     = entityHitResult.getEntity();
+			Vec3   viewVector = entity.getViewVector(1f);
+			if (ThirdPerson.ENTITY_AGENT.getRawPlayerEntity().getEyePosition().distanceTo(entityHitResult.getLocation()) > ThirdPersonConstants.MAX_INTERACTION_DISTANCE) {
+				hitResult = BlockHitResult.miss(hitResult.getLocation(), Direction.getNearest(viewVector.x, viewVector.y, viewVector.z), new BlockPos(hitResult.getLocation()));
+			}
+		} else if (hitResult instanceof BlockHitResult blockHitResult && hitResult.getType() != HitResult.Type.MISS) {
+			BlockPos blockPos         = blockHitResult.getBlockPos();
+			Vec3     centerOfBlockPos = Vec3.atCenterOf(blockPos);
+			if (ThirdPerson.ENTITY_AGENT.getRawPlayerEntity().getEyePosition().distanceTo(centerOfBlockPos) > ThirdPersonConstants.MAX_INTERACTION_DISTANCE) {
+				hitResult = BlockHitResult.miss(hitResult.getLocation(), blockHitResult.getDirection(), new BlockPos(hitResult.getLocation()));
+			}
+		}
+		ci.setReturnValue(hitResult);
 	}
 }
