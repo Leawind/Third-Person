b1088ab3a8c4bc2d6d979e31c72694e142c2c73d
leawind <leawind@yeah.net>
2024-03-08 22:34:44 +0800

    add dependency MixinExtras

--- common/src/main/java/net/leawind/mc/thirdperson/mixin/MouseHandlerMixin.java
+++ common/src/main/java/net/leawind/mc/thirdperson/mixin/MouseHandlerMixin.java
@@ -1,6 +1,7 @@
 package net.leawind.mc.thirdperson.mixin;
 
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.leawind.mc.thirdperson.ThirdPerson;
 import net.leawind.mc.thirdperson.ThirdPersonEvents;
 import net.leawind.mc.thirdperson.ThirdPersonStatus;
@@ -11,7 +12,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value=MouseHandler.class, priority=2000)
@@ -46,12 +46,13 @@ public class MouseHandlerMixin {
 	 * @param dx x轴角度（俯仰角）变化量
 	 * @param dy y轴角度（偏航角）变化量
 	 */
-	@Redirect(method="turnPlayer()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/player/LocalPlayer;turn(DD)V"))
-	public void turnPlayer_invoke (LocalPlayer instance, double dy, double dx) {
+	@WrapWithCondition(method="turnPlayer()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/player/LocalPlayer;turn(DD)V"))
+	public boolean turnPlayer_invoke (LocalPlayer instance, double dy, double dx) {
 		if (ThirdPerson.isAvailable() && ThirdPersonStatus.isRenderingInThirdPerson() && !ThirdPersonStatus.shouldCameraTurnWithEntity()) {
 			ThirdPerson.CAMERA_AGENT.onCameraTurn(dy, dx);
 		} else {
 			ThirdPerson.ENTITY_AGENT.getRawPlayerEntity().turn(dy, dx);
 		}
+		return true;
 	}
 }
