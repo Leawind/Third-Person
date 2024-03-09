1e735777e26bb81b4c021632ef1b8a4734b45408
leawind <leawind@yeah.net>
2024-03-09 23:04:15 +0800

    use ModifyExpressionValue instead of Redirect

--- common/src/main/java/net/leawind/mc/thirdperson/mixin/GuiMixin.java
+++ common/src/main/java/net/leawind/mc/thirdperson/mixin/GuiMixin.java
@@ -1,23 +1,16 @@
 package net.leawind.mc.thirdperson.mixin;
 
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.leawind.mc.thirdperson.ThirdPersonStatus;
-import net.minecraft.client.CameraType;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value=Gui.class, priority=2000)
 public class GuiMixin {
-	/**
-	 * 重定向 isFirstPerson()Z 方法
-	 * <p>
-	 * 原本仅在第一人称下显示准星，现在第三人称下也可以显示准星
-	 */
-	@Redirect(method="renderCrosshair(Lnet/minecraft/client/gui/GuiGraphics;)V", at=@At(value="INVOKE", target="Lnet/minecraft/client/CameraType;isFirstPerson ()Z"))
-	public boolean renderCrosshair_invoke (CameraType instance) {
-		return ThirdPersonStatus.shouldRenderCrosshair() || Minecraft.getInstance().options.getCameraType().isFirstPerson();
+	@ModifyExpressionValue(method="renderCrosshair", at=@At(value="INVOKE", target="Lnet/minecraft/client/CameraType;isFirstPerson()Z"))
+	public boolean r (boolean isFirstPersonReally) {
+		return isFirstPersonReally || ThirdPersonStatus.shouldRenderCrosshair();
 	}
 }
