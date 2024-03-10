3d3964470130afd6decbf9fb6b84ca118ed431d1
leawind <leawind@yeah.net>
2024-03-10 17:47:45 +0800

    add comments

--- common/src/main/java/net/leawind/mc/thirdperson/mixin/ItemMixin.java
+++ common/src/main/java/net/leawind/mc/thirdperson/mixin/ItemMixin.java
@@ -15,6 +15,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value=Item.class, priority=2000)
 public class ItemMixin {
+	/**
+	 * 当使用船、水瓶、水桶、末影之眼、刷怪蛋、PlaceOnWaterBlockItem 时，计算视线落点。
+	 */
 	@Inject(method="getPlayerPOVHitResult", at=@At("HEAD"), cancellable=true)
 	private static void getPlayerPOVHitResult (Level level, Player player, ClipContext.Fluid fluidShape, CallbackInfoReturnable<BlockHitResult> ci) {
 		if (ThirdPerson.isAvailable() && ThirdPersonStatus.isRenderingInThirdPerson()) {
