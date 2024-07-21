57736b78dd1d30447a6e4b1df251f14c85ff345b
leawind <leawind@yeah.net>
2024-07-21 15:47:32 +0800

    style: add @Contract anno

--- common/src/main/java/net/leawind/mc/util/OptionalFunction.java
+++ common/src/main/java/net/leawind/mc/util/OptionalFunction.java
@@ -1,10 +1,14 @@
 package net.leawind.mc.util;
 
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.Supplier;
 
 public interface OptionalFunction<T> {
-	static <E> OptionalFunction<E> of (Supplier<E> supplier, Supplier<Boolean> availablePredicate) {
+	@Contract(value="_, _ -> new", pure=true)
+	static <E> @NotNull OptionalFunction<E> of (Supplier<E> supplier, Supplier<Boolean> availablePredicate) {
 		return new OptionalFunction<>() {
 			@Override
 			public E get () {
