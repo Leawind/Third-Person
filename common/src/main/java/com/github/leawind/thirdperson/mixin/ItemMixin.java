463bb3bacbb65a296231fc00c0d27a5bb6aa0491
leawind <leawind@yeah.net>
2024-10-01 13:58:03 +0800

    remove unused class ItemMixin

--- common/src/main/java/com/github/leawind/thirdperson/mixin/ItemMixin.java
+++
@@ -1,9 +0,0 @@
-package com.github.leawind.thirdperson.mixin;
-
-
-import net.minecraft.world.item.Item;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value=Item.class, priority=2000)
-public class ItemMixin {
-}
