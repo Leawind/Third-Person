93afeccd526e89fdf671f77de0a8b38d42cda7cc
Leawind <leawind@yeah.net>
2026-08-03 21:51:16 +0800

    refactor: no longer use internal things in Perspective API

--- src/main/java/io/github/leawind/thirdperson/internal/logic/base/camera/CameraOffsetSqueeze.java
+++ src/main/java/io/github/leawind/thirdperson/internal/logic/base/camera/CameraOffsetSqueeze.java
@@ -1,6 +1,5 @@
 package io.github.leawind.thirdperson.internal.logic.base.camera;
 
-import io.github.leawind.perspectiveapi.internal.utils.smooth.Blenders;
 import java.util.Objects;
 import java.util.function.DoubleUnaryOperator;
 import org.joml.Quaternionfc;
@@ -12,7 +11,7 @@ final class CameraOffsetSqueeze {
   static final double CENTERED_PITCH_DEGREES = 89.8;
 
   /// Maps normalized squeeze progress to centered progress. Both input and output use [0, 1].
-  static final DoubleUnaryOperator TRANSITION_FUNCTION = v -> Blenders.easeIn((float) v);
+  static final DoubleUnaryOperator TRANSITION_FUNCTION = v -> v * v;
 
   private CameraOffsetSqueeze() {}
 
